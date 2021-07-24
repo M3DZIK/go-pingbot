cf0bb1c0a60810d55c25fba9508537a0a3691ff2
Medzik <8584366-Medzik@users.noreply.gitlab.com>
2021-07-24 21:19:22 +0000

    v1.3.1
    - Added gitpod Dockerfile
    - Changes in dependencies

--- update/update.go
+++ update/update.go
@@ -3,8 +3,8 @@ package update
 import (
 	"os"
 
-	"github.com/blang/semver"
-	"github.com/rhysd/go-github-selfupdate/selfupdate"
+	"github.com/MedzikUser/go-github-selfupdate/selfupdate"
+	"github.com/blang/semver/v4"
 	"gitlab.com/gaming0skar123/go/pingbot/common"
 	"gitlab.com/gaming0skar123/go/pingbot/config"
 )
@@ -31,7 +31,7 @@ func Update() {
 		return
 	}
 
-	err = selfupdate.UpdateTo(latest.AssetURL, exe)
+	err = selfupdate.UpdateTo(latest.AssetURL, exe, "pingbot.out")
 	if common.CheckErr(err, "update binary") {
 		return
 	}
