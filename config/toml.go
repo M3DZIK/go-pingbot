fe16a2ab625f02fbf14c42d8ef8d05a17fa500c5
Medzik <87065584+MedzikUser@users.noreply.github.com>
2021-08-07 20:03:26 +0000

    feat(config): auto download schema config

--- config/toml.go
+++ config/toml.go
@@ -1,6 +1,9 @@
 package config
 
 import (
+	"io"
+	"net/http"
+	"os"
 	"time"
 
 	"github.com/BurntSushi/toml"
@@ -39,5 +42,34 @@ var Toml tomlConfig
 func init() {
 	_, err := toml.DecodeFile("./config.toml", &Toml)
 
-	common.CheckErr(err, "decode toml config")
+	if common.CheckErr(err, "decode toml config") {
+		if err.Error() == "open ./config.toml: no such file or directory" {
+			err := DownloadFile("config.toml", "https://github.com/"+GH_Repo+"/raw/main/config.schema.toml")
+
+			if !common.CheckErr(err, "download default config") {
+				_, err = toml.DecodeFile("./config.toml", &Toml)
+				common.CheckErr(err, "decode toml config")
+			}
+		}
+	}
+}
+
+func DownloadFile(filepath string, url string) error {
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
